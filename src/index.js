8180e5559338da6e2819e9786b118ff8256972d5
Slava Manzhela <slavamanzh@gmail.com>
2023-01-24 18:12:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let expr2 = '';
+    for (let i=0; i<expr.length; i+=2) {
+        if (i%10===0){
+            expr2+=' ';
+        }
+        if (expr[i]==='0') {
+            expr2+='';
+        } else if ((expr[i]==='1')&& (expr[i+1]==='1')){
+            expr2+='-';
+        } else if ((expr[i]==='1')&& (expr[i+1]==='0')) {
+            expr2+='.';
+        } else {
+            expr2+=' ';
+            i=i+8;
+        }
+    }
+
+    var words = (expr2).split('  ');
+    var letters = words.map((w) => w.split(' '));
+    var decoded = [];
+
+    for(var i = 0; i < letters.length; i++){
+        decoded[i] = [];
+        for(var x = 0; x < letters[i].length; x++){
+            if(MORSE_TABLE[letters[i][x]]){
+                decoded[i].push(MORSE_TABLE[letters[i][x]] );
+            }
+        }
+    }
+
+  return decoded.map(arr => arr.join('')).join(' ');
+
 }
 
 module.exports = {
